3915fe2d645389e88b715c4a2071edb50aecb689
csny <masaono_13@msn.com>
2015-04-13 10:11:10 +0900

    アップロード

---
+++ SpeedTest.java
@@ -0,0 +1,62 @@
+package local.orenchi;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.stream.IntStream;
+
+public class SpeedTest {
+
+	// debug variable
+	static long allcounter;
+	static int zerocounter;
+	static int loopcounter;
+	
+	private static void decreaseRecursion(long cnt) {
+		
+		allcounter++;
+		if (cnt == 0){
+			zerocounter++;
+			return;
+		} else {
+			decreaseRecursion(--cnt);
+		}
+	}
+	private static boolean initCalc1() {
+		for (int i=0;i<100;i++){
+			for (int j=0;j<100;j++){
+				decreaseRecursion(100000);
+				loopcounter++;
+			}
+		}
+		return true;
+	}
+	private static boolean initCalc2() {
+		IntStream.range(0, 100).forEach(i -> {
+			IntStream.range(0, 100).forEach(j -> {
+				decreaseRecursion(100000);
+				loopcounter++;
+			});
+		});
+		return true;
+	}
+	
+	public static void main(String[] args) {
+		
+		Instant start = Instant.now();
+		System.out.println("Start!");
+		allcounter=0;
+		zerocounter=0;
+		loopcounter=0;
+		
+		if (initCalc2()){
+			System.out.println("End");
+			Instant end = Instant.now();
+			Duration elapse = Duration.between(start, end);
+			System.out.println(elapse + "[s]");
+			System.out.println("allcnt:"+allcounter+" zerocnt:"+zerocounter+" loopcnt:"+loopcounter);
+		} else {
+			System.out.println("Error!");
+		}
+	}
+
+}
